d085de7973555e9ef0486c2c7dfca44846ec5a7b
WingarWang <89078123+WingarWang@users.noreply.github.com>
2021-10-08 22:35:54 -0400

    Add files via upload

---
+++ visualization_function.R
@@ -0,0 +1,76 @@
+#function1
+plot1<-function(x){
+  try(if(!is.data.frame(x)) stop("Input data must be a data frame."))
+  
+  color=ifelse(x$country=="China","red","blue")
+  group<-ifelse(x$country=="China",1,2)
+  plota<-ggplot(data=x) +
+    geom_line(aes(x=year,y=gdp,colour=country,group=group))+
+    geom_point(aes(x=year,y=gdp,colour=color))+
+    labs(x="YEAR",
+         y="GDP",
+         subtitle="Line Chart: US vs China of GDP",
+         caption="Source:Gapminder")
+  return(plota)
+}
+#input: plot1(tidy_use)
+
+
+#function2
+plot2<-function(x){
+  try(if(!is.data.frame(x)) stop("Input data must be a data frame."))
+  
+  plotb<-ggplot(data=x) +
+    geom_bar(aes(x=year,y=gdp,fill=country),position="dodge",stat="identity")+
+    labs(x="YEAR",
+         y="GDP",
+         subtitle="Bar graph: US vs China of GDP",
+         caption="Source: Gapminder")
+  return(plotb)
+}
+#input: plot2(tidy_use)
+
+
+#function3
+plot3<-function(x){
+  try(if(!is.data.frame(x)) stop("Input data must be a data frame."))
+  
+  plotc<-ggplot(data=x, aes(x=co2, fill=country)) +
+    geom_density(alpha=0.3)
+  return(plotc)
+}
+#input: plot3(tidy_use)
+
+#This task is in Assignment 1 "hand_fuction"
+
+##########################################################
+
+#plot1, line chart
+color=ifelse(tidy_use$country=="China","red","blue")
+group<-ifelse(tidy_use$country=="China",1,2)
+plot1<-ggplot(data=tidy_use) +
+  geom_line(aes(x=year,y=gdp,colour=country,group=group))+
+  geom_point(aes(x=year,y=gdp,colour=color))+
+  labs(x="YEAR",
+       y="GDP",
+       subtitle="Line Chart: US vs China of GDP",
+       caption="Source:Gapminder")
+plot1
+
+
+#plot2, bar graph
+plot2<-ggplot(data=tidy_use) +
+  geom_bar(aes(x=year,y=gdp,fill=country),position="dodge",stat="identity")+
+  labs(x="YEAR",
+       y="GDP",
+       subtitle="Bar graph: US vs China of GDP",
+       caption="Source: Gapminder")
+plot2
+
+
+#plot3, density 
+plot3<-ggplot(data=tidy_use, aes(x=co2, fill=country)) +
+  geom_density(alpha=0.3)
+plot3
+
+
---
+++ wrangling_code.R
@@ -0,0 +1,45 @@
+setwd("C:/Users/L/Downloads")
+
+install.packages("readxl")
+library(readxl)
+
+install.packages("xlsx")
+library("xlsx")
+
+library(tidyverse)
+
+library(tidyr)
+library(dplyr)
+
+install.packages("reader")
+library(reader)
+
+##########################################################
+
+#read excel
+gdp_all<-read_xlsx("gdp_total_yearly_growth.xlsx")
+co2_all<-read_excel("co2_intensity_of_economic_output_kg_co2_per_2011_ppp_of_gdp.xlsx")
+
+as_tibble(gdp_all)
+as_tibble(co2_all)
+
+#select 2000 to 2012
+gdp<-select(gdp_all,"country","2000":"2012")
+co2<-select(co2_all,"country","2000":"2012")
+
+##########################################################
+
+#use pivot_long to tidy the data
+gdp_tidy<- gdp %>% 
+  pivot_longer(!country,names_to="year",values_to="gdp")
+
+co2_tidy<- co2 %>% 
+  pivot_longer(!country,names_to="year",values_to="co2")
+
+#use inner_join to combine the data
+tidy<- co2_tidy %>% inner_join(gdp_tidy)
+
+#select two countries
+tidy_use<- filter(tidy,country=="United States" | country=="China")
+
+
